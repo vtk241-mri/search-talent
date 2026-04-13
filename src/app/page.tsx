d229ef258c1a9561aa6f892c7120d994936fe221
Rostyslav <vtk241_mri@student.ztu.edu.ua>
2026-04-13 15:27:11 +0300

    Додано SpeedInsights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { defaultLocale } from "@/lib/i18n/config";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootRedirectPage() {
+  <SpeedInsights />;
   redirect(`/${defaultLocale}`);
 }
